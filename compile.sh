a94de54727cd36bf6acb5a2d919b3e5d9c1057b0
Jason Schulte <jasonschulte@gmail.com>
2024-03-06 07:47:40 -0600

    move compile script

---
+++ compile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+mkdir -pv build
+cd build
+cmake ../ \
+  -DCMAKE_INSTALL_PREFIX="/azerothcore/"  \
+  -DAPPS_BUILD="all"                      \
+  -DTOOLS_BUILD="none"                    \
+  -DSCRIPTS="static"                      \
+  -DMODULES="static"                      \
+  -DWITH_WARNINGS="1"                     \
+  -DCMAKE_CXX_COMPILER="/usr/bin/clang++" \
+  -DCMAKE_C_COMPILER="/usr/bin/clang"     \
+  -DCMAKE_CXX_COMPILER_LAUNCHER="ccache"  \
+  -DCMAKE_C_COMPILER_LAUNCHER="ccache"    \
+
+#cmake ../ -DCMAKE_INSTALL_PREFIX=$HOME/azeroth-server/ -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DWITH_WARNINGS=1 -DTOOLS_BUILD=all -DSCRIPTS=static -DMODULES=static
+make -j 32
