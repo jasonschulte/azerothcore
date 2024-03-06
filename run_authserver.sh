9830b54bbda02f3df0a47c39813b418eb7149e21
Jason Schulte <jasonschulte@gmail.com>
2024-03-06 07:53:24 -0600

    fix run_authserver.sh

--- run_authserver.sh
+++ run_authserver.sh
@@ -4,10 +4,11 @@ source .env
 set +o allexport
 
 cd env/dist/bin
+./authserver
 
-while true; do 
-  ./authserver &
-  PID=$!
-  inotifywait -e modify -e move -e create -e delete -e attrib -r ../etc -r ./
-  kill $PID
-done
+# while true; do 
+#   ./authserver &
+#   PID=$!
+#   inotifywait -e modify -e move -e create -e delete -e attrib -r ../etc -r ./
+#   kill $PID
+# done
